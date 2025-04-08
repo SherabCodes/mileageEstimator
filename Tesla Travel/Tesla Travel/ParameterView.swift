fe341c90c4c8e43e8eb84d16ba0efb46d2ec9d5d
Sienna Ni <sn3866@nyu.edu>
2025-04-08 16:02:17 +0800

    Add files via upload

---
+++ Tesla Travel/Tesla Travel/ParameterView.swift
@@ -0,0 +1,7 @@
+//
+//  ParameterView.swift
+//  Tesla Travel
+//
+//  Created by Sienna Ni on 2025/4/7.
+//
+
---
+++ Tesla Travel/Tesla Travel/Tesla_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesla_TravelApp.swift
+//  Tesla Travel
+//
+//  Created by Sienna Ni on 2025/4/7.
+//
+
+import SwiftUI
+
+@main
+struct Tesla_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
